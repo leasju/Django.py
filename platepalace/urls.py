0cc8e314a0a711ebb260019f40926369bb61d23b
jslea <jusouzaleandro@gmail.com>
2024-09-15 11:54:13 -0300

    Remoção do 'contato' e 'sobre'

--- platepalace/urls.py
+++ platepalace/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from platepalace.views import home, contato, sobre
+from platepalace.views import home
 
 # dominio.com/platepalace/
 urlpatterns = [
     path('', home ), # Home
-    path('sobre/', sobre), # /sobre/
-    path('contato/', contato) # /contato/
+   
 ]
--- platepalace/views.py
+++ platepalace/views.py
@@ -3,13 +3,5 @@ from django.http import HttpResponse
 
 # HTTP REQUEST
 def home(request): #cliente faz uma request ->  servidor retorna uma response
-    return HttpResponse('HOME')
+    return render(request, 'platepalace/home.html')
     # return  HTTP Response
-    
-def contato(request): 
-    return HttpResponse('CONTATO')
-
-def sobre(request): 
-    return HttpResponse('SOBRE')
-
-   
